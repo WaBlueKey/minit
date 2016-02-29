2cbf4a3bb2f6da20819f1cbbbeebb4afb14e023b
stvngrcia <stevengarciaacuna@gmail.com>
2016-02-28 19:08:40 -0800

    created post and replay boxes and first js file

--- post_a_status.js
+++ post_a_status.js
@@ -1,3 +1,14 @@
-function displayHidden() {
-    document.getElementsByClassName("new_comment").style.visibility = "hidden";   
-}
+window.onload = function () {
+	var el = document.getElementById('replaystatus');
+	el.onclick = function () {
+		/*alert("Hello! I am an alert box!!");*/
+		 document.getElementById('replayform').style.display='block'; 
+	};
+	var e = document.getElementById('playstatus');
+	e.onclick = function () {
+
+		document.getElementById('playform').style.display='block'; 
+	};
+};
+
+/*-------------------------------------------------------------------*/
