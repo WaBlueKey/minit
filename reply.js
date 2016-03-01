d3f1b08392d485afcde5a8a480fd85fce5e0c3a3
steven Garcia <27@p3-5.hbtn.io>
2016-02-29 18:32:14 -0800

    adding reply functionality

--- reply.js
+++ reply.js
@@ -1,9 +1,18 @@
 window.addEventListener("load",  function () {
-    var e = document.getElementById('playstatus');
+    var e = document.querySelectorAll('.playstatus');
+    
+    for (var a= 0; a < e.length; a++){
+    /*
     e.onclick = function () {
 
         var item_num = playstatus.getAttribute('data-num');
         document.getElementById('playform' + item_num).style.display = 'block';
-    };
+    };*/
+    e[a].addEventListener("click",function(){
+        var item_num = this.getAttribute('data-num');
+         document.getElementById('playform' + item_num).style.display = 'block';
+    } ); 
+    }
 
 });
+
