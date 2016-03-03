4ac84dc69a447b0afc0be1e3cc60aeb4e2a05c81
stvngrcia <stevengarciaacuna@gmail.com>
2016-03-02 16:50:53 -0800

    added a function name toggle, that makes the post status and post a replay link toggle back and forth

--- post_a_status.js
+++ post_a_status.js
@@ -2,7 +2,17 @@ window.onload = function () {
     var el = document.getElementById('replaystatus');
     el.onclick = function () {
         /*alert("Hello! I am an alert box!!");*/
-        document.getElementById('replayform').style.display = 'block';
+        var h = document.getElementById('replayform')//.style.display = 'block';
+		
+		/*if (h.style.display == '')
+			{
+				h.style.display = 'block';
+			}
+		else
+		{
+			h.style.display = '';
+		}*/
+		h.toggle(); 
     };
 
 
