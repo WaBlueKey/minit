f983642d2afd5b5eb6e2dd67ed26ffc9574b9ee8
stvngrcia <stevengarciaacuna@gmail.com>
2016-03-18 21:16:55 -0700

    reply works inside load more statuses. need to fix problems with reply

--- load_more.js
+++ load_more.js
@@ -1,17 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-	
-var elemn = document.getElementById("extra_button");
-	
-elemn.onclick =function() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("extra_section").innerHTML = xhttp.response;
-    }
-  };
-document.getElementById("extra_section").style.background="white"; 
-  xhttp.open("GET", "statuses-1.html", true);
-  xhttp.send();
-}
-});
+function load_more() {
+	var elemn = document.getElementById("extra_button");
+	console.log(elemn);
+	elemn.onclick = function () {
+		var xhttp = new XMLHttpRequest();
 
+		xhttp.onreadystatechange = function () {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				document.getElementById("extra_section").innerHTML = xhttp.response;
+				reply();
+			}
+		};
+		document.getElementById("extra_section").style.background = "white";
+		xhttp.open("GET", "statuses-1.html", true);
+		xhttp.send();
+
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	load_more();
+	reply();
+
+});
--- reply.js
+++ reply.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-	var e = document.querySelectorAll('.playstatus');
-	var item_num = 0;
-	for (var a = 0; a < e.length; a++) {
-		e[a].addEventListener("click", function () {
-			item_num = this.getAttribute('data-num');
-			
-			var h = document.getElementById('playform' + item_num)
+function reply() {
+ 	var e = document.querySelectorAll('.playstatus');
+ 	var item_num = 0;
+ 	for (var a = 0; a < e.length; a++) {
+ 		e[a].addEventListener("click", function () {
+ 			item_num = this.getAttribute('data-num');
 
-			h.toggle(); 
+ 			var h = document.getElementById('playform' + item_num)
 
-		});
-	}
+ 			h.toggle();
 
-},false);
+ 		});
+ 	}
+
+ }
