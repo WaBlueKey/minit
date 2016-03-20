23888b09613e295da27709ea55e8b8edc6133a7b
stvngrcia <stevengarciaacuna@gmail.com>
2016-03-20 12:04:53 -0700

    Changed functinalities on the see more button, now it changes colors and displays loading when waiting for a response

--- load_more.js
+++ load_more.js
@@ -1,17 +1,24 @@
 function load_more() {
-	
+
 	var elemn = document.getElementById("extra_button");
-					  
+
 	elemn.onclick = function () {
+		document.getElementById("extra_button").style.background = "#FFDB58";
+		document.getElementById("extra_button").innerHTML = "LOADING ..."; 
+		/*disabling the link so that users dont keep clicking on it*/
+		document.getElementById("extra_button").disable = true; 
 		
-		ajaxGet("statuses-1.html", function (response) {
-			document.getElementById("extra_section").innerHTML = response;
-		});
-		
+		setTimeout(function () {
+			ajaxGet("statuses-1.html", function (response) {
+				document.getElementById("extra_section").innerHTML = response;
+			});
+		}, 2000);
+
+
 	}
 }
 
-document.addEventListener("DOMContentLoaded",function () {
+document.addEventListener("DOMContentLoaded", function () {
 	load_more();
-	reply(); 
+	reply();
 });
