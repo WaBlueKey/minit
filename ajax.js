5774168cf0eae67077adf03ec25ccaf1e9de6f9a
stvngrcia <stevengarciaacuna@gmail.com>
2016-03-21 13:46:22 -0700

    fixed problem with post a reply when loading more comments

--- ajax.js
+++ ajax.js
@@ -1,13 +1,14 @@
 function ajaxGet(url, onSuccess) {
 
 	var xhttp = new XMLHttpRequest();
-	
+
 	xhttp.onreadystatechange = function () {
-		
+
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			onSuccess(xhttp.responseText); 
-			
-			reply();
+			onSuccess(xhttp.responseText);
+			reply_extra();
+
+
 		}
 	};
 	/*this is to handle the backgound in extra comments and keep the background on the see more statuses bar*/
--- reply_extra.js
+++ reply_extra.js
@@ -1,11 +1,11 @@
  function reply_extra() {
- 	var e = document.querySelectorAll('.playstatus');
+ 	var e = document.querySelectorAll('.playstatu');
  	var item_num = 0;
  	for (var a = 0; a < e.length; a++) {
  		e[a].addEventListener("click", function () {
  			item_num = this.getAttribute('data-num');
 
- 			var h = document.getElementById('playform' + item_num)
+ 			var h = document.getElementById('playfor' + item_num)
 
  			h.toggle();
 
