30401d648019aa4384de20d305338f288058ec77
Zee Adams <wabluekey@gmail.com>
2016-03-21 18:31:54 -0400

    added new html and js files

---
+++ Holberton_Projects/2016/minit/load_more.js
@@ -0,0 +1,4 @@
+function load_more() {
+    "use strict";
+    document.getElementById("extra_statuses").innerHTML = '<object type="type/html" data="statuses-1.html" ></object>';
+}
---
+++ Holberton_Projects/2016/minit/post_a_status.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    "use strict";
+    var el = document.getElementById('replaystatus');
+    el.onclick = function () {
+        /*alert("Hello! I am an alert box!!");*/
+        var h = document.getElementById('replayform');//.style.display = 'block';
+		
+		/*if (h.style.display == '')
+			{
+				h.style.display = 'block';
+			}
+		else
+		{
+			h.style.display = '';
+		}*/
+		h.toggle();
+    };
+
+
+};
+
+/*-------------------------------------------------------------------*/
